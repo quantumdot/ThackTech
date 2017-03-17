18a3968a62ead9990891efd330a5f47db1f96e1c
quantumdot <thackray@rutgers.edu>
2017-03-17 12:52:21 -0400

    Seems like glob should not be in the install_requires because it is built in?

--- setup.py
+++ setup.py
@@ -1,29 +1,29 @@
 from setuptools import setup
 
-setup(name='ThackTech',
-      version='0.1',
-      description='Libraries for process management, pipelineing, and other goodies.',
-      url='https://github.com/quantumdot/ThackTech',
-      author='Joshua K. Thackray',
-      author_email='thackray@rutgers.edu',
-      license='MIT',
-      #impackages=['ThackTech'],
-      install_requires=[
-		  'numpy',
-		  'scipy',
-          'intervaltree',
-		  'pysam',
-          'pybedtools',
-		  'glob',
-		  'dill',
-		  'multiprocess',
-		  'tabulate',
-		  #'sqlite3',
-      ],
-      scripts=[
-          "scripts/bedToBedGraph.py",
-          "scripts/wigToBedGraph.py",
-          "scripts/cleanBedGraph.py",
-          "scripts/profiler.py"
-      ],
-      zip_safe=False)
+
+setup(  name='ThackTech',
+        version='0.1',
+        description='Libraries for process management, pipelineing, and other goodies.',
+        url='https://github.com/quantumdot/ThackTech',
+        author='Joshua K. Thackray',
+        author_email='thackray@rutgers.edu',
+        license='MIT',
+        install_requires=[
+            'numpy',
+            'scipy',
+            'intervaltree',
+            'pysam',
+            'pybedtools',
+            'dill',
+            'multiprocess', #this is different than the built in multiprocessing!
+            'tabulate',
+            #'glob', 
+            #'sqlite3',
+        ],
+        scripts=[
+            "scripts/bedToBedGraph.py",
+            "scripts/wigToBedGraph.py",
+            "scripts/cleanBedGraph.py",
+            "scripts/profiler.py"
+        ],
+        zip_safe=False)
