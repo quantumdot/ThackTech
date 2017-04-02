6ed4d7d125e8883f20512f8981ae2a0d88b074fb
quantumdot <thackray@rutgers.edu>
2017-04-02 00:52:02 -0400

    finish refactoring RemoveDecompressedFiles

--- ThackTech/Pipelines/PipelineModules/RemoveDecompressedFiles.py
+++ ThackTech/Pipelines/PipelineModules/RemoveDecompressedFiles.py
@@ -9,9 +9,11 @@ class RemoveDecompressedFiles(PipelineModule):
 	#end __init__()
 	
 	def run(self, cxt):
-		for f in cxt.sample.get_attribute('decompressed_files'):
-			cxt.log.write("\t-> Removing decompressed file %s....\n" % (f[],))
+		for file_group in cxt.sample.get_attribute('decompressed_files'):
+			decompressed_file = cxt.sample.find_files(lambda f: f.fullpath == file_group[0])[0]
+			decompressed_file._set_path(file_group[1])
+			cxt.log.write("\t-> Removing decompressed file {}....\n".format(file_group[0]))
 			cxt.log.flush()
-			os.remove(f)
+			os.remove(file_group[0])
 	#end run()
 #end class GeneratePseudoreplicates
