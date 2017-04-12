03961d357e43d2230186512012281e706b3cad26
quantumdot <thackray@rutgers.edu>
2017-04-12 18:27:12 -0400

    Added new Modules: FastQC and PeatTrimmer
    added FASTQC to bowtie alignment pipeline

---
+++ ThackTech/Pipelines/PipelineModules/FastQC.py
@@ -0,0 +1,66 @@
+import os
+import subprocess
+from ThackTech import filetools
+from ThackTech.Pipelines import PipelineModule
+from ThackTech.Pipelines.ModuleParameter import ModuleParameter
+from ThackTech.Pipelines.FileInfo import FileInfo, FileContext
+
+
+class FastQC(PipelineModule):
+	
+	def __init__(self, **kwargs):
+		super(FastQC, self).__init__('FastQC', 'FASTQ Quality Metrics', **kwargs)
+		
+		self.add_parameter(ModuleParameter('kmers', int, 7, desc="Specifies the length of Kmer to look for in the Kmer content module. Specified Kmer length must be between 2 and 10."))
+		self.add_parameter(ModuleParameter('limits', str, None, nullable=True, desc="Specify a location for the limits file (--limits option in fastqc)"))
+		self.add_parameter(ModuleParameter('adapters', str, None, nullable=True, desc="Specify a location for the adapters file (--adapters option in fastqc)"))
+		self.add_parameter(ModuleParameter('contaminants', str, None, nullable=True, desc="Specify a location for the contaminants file (--contaminants option in fastqc)"))
+		
+		self._name_resolver('fastqs')
+	#end __init__()
+	
+	def tool_versions(self):
+		return {
+			'fastqc': self._call_output("fastqc --version 2>&1 | perl -ne 'if(m/([\d\.]+)/){ print $1 }'", shell=True, stderr=subprocess.STDOUT)
+		}
+	#end tool_versions()
+	
+	def run(self, cxt):
+		dest_dir = os.path.join(cxt.sample.dest, 'fastqc')
+		filetools.ensure_dir(dest_dir)
+		
+		out_files = []
+		fastqc_args = [
+			'fastqc',
+			'--quiet', #disable progress reporting
+			'--threads', str(self.processors),
+			'--outdir',  dest_dir,
+			'--kmers', self.get_parameter_value_as_string('kmers')
+		]
+		if self.get_parameter_value('limits') is not None:
+			fastqc_args += ['--limits', self.get_parameter_value_as_string('limits')]
+			
+		if self.get_parameter_value('adapters') is not None:
+			fastqc_args += ['--adapters', self.get_parameter_value_as_string('adapters')]
+		
+		if self.get_parameter_value('contaminants') is not None:
+			fastqc_args += ['--contaminants', self.get_parameter_value_as_string('contaminants')]
+			
+			
+		for f in self.resolve_input('fastqs', cxt):
+			fastqc_args.append(f.fullpath)
+			of = FileInfo(os.path.join(dest_dir, "{}_fastqc.html".format(f.filename)), FileContext.from_module_context(cxt, "FastQC_report"))
+			of.companions.append(FileInfo(os.path.join(dest_dir, "{}_fastqc.zip".format(f.filename)), FileContext.from_module_context(cxt, "FastQC_report_support_files")))
+			out_files.append(of)
+		
+
+		cxt.log.write("Running FastQC for "+cxt.sample.name+"......\n")
+		cxt.log.write("\n..............................................\n")
+		cxt.log.write(" ".join(fastqc_args))
+		cxt.log.write("\n..............................................\n")
+		cxt.log.flush()
+		self._run_subprocess(fastqc_args, stderr=subprocess.STDOUT, stdout=cxt.log)
+		
+		return out_files
+	#end run()
+#end class BamFingerprint
---
+++ ThackTech/Pipelines/PipelineModules/PeatTrimmer.py
@@ -0,0 +1,81 @@
+import os
+import subprocess
+from ThackTech import filetools
+from ThackTech.Pipelines import PipelineModule, ModuleParameter
+from ThackTech.Pipelines.FileInfo import FileInfo, FileContext
+
+
+class PeatTrimmer(PipelineModule):
+	
+	def __init__(self, **kwargs):
+		super(PeatTrimmer, self).__init__('PEAT', 'Trim reads with PEAT', **kwargs)
+		
+		self.add_parameter(ModuleParameter('l', int, 30, desc="Minimum gene fragment length, i.e. the fragment length for reverse complement check"))
+		self.add_parameter(ModuleParameter('r', float, 0.3, desc="Mismatch rate applied in first stage reverse complement scan"))
+		self.add_parameter(ModuleParameter('g', float, 0.6, desc="Mismatch rate applied in second stage gene portion check"))
+		self.add_parameter(ModuleParameter('a', float, 0.4, desc="Mismatch rate applied in second stage adapter portion check"))
+		
+		self._name_resolver('fastq')
+		self.set_parameters_from_config()
+	#end __init__()
+	
+	def tool_versions(self):
+		return {
+			'peat': self._call_output(["TrimmomaticSE", "-version"], stderr=subprocess.STDOUT)
+		}
+	#end tool_versions()
+	
+	def run(self, cxt):
+		if not cxt.sample.get_attribute('PE'):
+			raise("PEAT only works with paired-ended data!")
+		
+		outdir = os.path.join(cxt.sample.dest, 'peat')
+		filetools.ensure_dir(outdir) 
+
+		#trimlog_loc = os.path.join(outdir, cxt.sample.name+'.trimlog')
+		peat_args = [
+			'Trimmomatic' + ('PE' if cxt.sample.get_attribute('PE') else 'SE'),
+			'-n', str(self.processors),
+			'-l', self.get_parameter_value_as_string('l'),
+			'-r', self.get_parameter_value_as_string('r'),
+			'-g', self.get_parameter_value_as_string('g'),
+			'-a', self.get_parameter_value_as_string('a')
+		]
+		out_files = []
+		read_files = self.resolve_input('fastq', cxt)
+		
+		#Input Files
+		peat_args.append('-1')
+		peat_args.append([f for f in read_files if f.has_attribute_value("mate", 1)][0].fullpath)
+		peat_args.append('-2')
+		peat_args.append([f for f in read_files if f.has_attribute_value("mate", 2)][0].fullpath)
+		#Output Files
+		out_files.append(FileInfo(os.path.join(outdir, cxt.sample.name+'_R1.filtered.paired.fastq.gz'), 
+						 FileContext.from_module_context(cxt, "filtered_paired_reads"),
+						 mate=1, filtered=True, paired=True))
+		out_files.append(FileInfo(os.path.join(outdir, cxt.sample.name+'_R1.filtered.unpaired.fastq.gz'), 
+						 FileContext.from_module_context(cxt, "filtered_unpaired_reads"),
+						 mate=1, filtered=True, paired=False))
+		out_files.append(FileInfo(os.path.join(outdir, cxt.sample.name+'_R2.filtered.paired.fastq.gz'), 
+						 FileContext.from_module_context(cxt, "filtered_paired_reads"),
+						 mate=2, filtered=True, paired=True))
+		out_files.append(FileInfo(os.path.join(outdir, cxt.sample.name+'_R2.filtered.unpaired.fastq.gz'), 
+						 FileContext.from_module_context(cxt, "filtered_unpaired_reads"),
+						 mate=2, filtered=True, paired=False))
+		for f in out_files:
+			peat_args.append(f.fullpath)
+			
+
+			
+		
+		
+		cxt.log.write("\t-> Performing trimming of source sequences......")
+		cxt.log.write("\n..............................................\n")
+		cxt.log.write(" ".join(peat_args))
+		cxt.log.write("\n..............................................\n")
+		cxt.log.flush()
+		self._run_subprocess(peat_args, stderr=subprocess.STDOUT, stdout=cxt.log)
+		
+		return out_files
+	#end run()
+#end class Trimmomatic	
