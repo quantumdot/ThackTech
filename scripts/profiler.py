9ac4b339a1cc3b1c41a2f5911eeffe182de4a932
quantumdot <thackray@rutgers.edu>
2017-06-22 14:31:50 -0400

    .

--- scripts/profiler.py
+++ scripts/profiler.py
@@ -336,11 +336,11 @@ def main():
     sys.stderr.write("\n")
     
     #if we have multiple bed and multiple signals, add legand outside last avg plot
-    if 'avg' in gopts['args'].plot and len(args.sig) > 1 and len(args.bed) > 1:
-        last_avg_ax = get_plot_axes('leg', 0, 0, 0)
-        leg = last_avg_ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-        leg.get_frame().set_linewidth(0.1)
-        gopts['extra_artists'].append(leg)
+#     if 'avg' in gopts['args'].plot and len(args.sig) > 1 and len(args.bed) > 1:
+#         last_avg_ax = get_plot_axes('leg', 0, 0, 0)
+#         leg = last_avg_ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+#         leg.get_frame().set_linewidth(0.1)
+#         gopts['extra_artists'].append(leg)
     
     #add colorscale bar if necessary
     if 'heat' in gopts['args'].plot:
