d6ca9fb87e9302f9004f30b17b78bcf3d2b0b82a
quantumdot <thackray@rutgers.edu>
2017-07-14 15:07:30 -0400

    .

--- ThackTech/Plotting/intensity_plots.py
+++ ThackTech/Plotting/intensity_plots.py
@@ -131,11 +131,11 @@ def plot_radial_cumulative_sum(ax, dfs, colx, coly, labels):
         
         print "radial\t%sig"
         for j in np.linspace(0.0, 0.95, 20, dtype=float):
-            print "{}\t{}".format(j, df_s['cum_perc'][df_s['norm_x'>=j].argmax()])
+            print "{}\t{}".format(j, df_s.where(df_s['norm_x']>=j)['cum_perc'].min())
             
         print "%sig\tradial"
         for j in np.linspace(0.0, 0.95, 20, dtype=float):
-            print "{}\t{}".format(j, df_s['norm_x'][df_s['cum_perc'>=j].argmax()])
+            print "{}\t{}".format(j, df_s.where(df_s['cum_perc']>=j)['norm_x'].min())
         
         ax.plot(df_s['norm_x'], df_s['cum_perc'], label=labels[i], color=color)
 
