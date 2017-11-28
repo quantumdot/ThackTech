513ff7dccf053e56baf82c248d27b87b98d02989
quantumdot <thackray@rutgers.edu>
2017-11-28 12:37:52 -0500

    clean up and mostly documentation/argparse tweaks

---
+++ ThackTech/Plotting/layout.py
@@ -0,0 +1,31 @@
+
+
+
+# class PlotLayout:
+    # def __init__(self):
+        # self.items = []
+    # #end __init__()
+
+    # def finalize(self):
+        # total_rows = 0
+        # total_cols = 0
+        # groups = set()
+        # for item in self.items:
+            # total_rows = max(total_rows, item.relative_row)
+            # total_cols = max(total_cols, item.relative_col)
+            # for s in item.samples:
+                # groups.add(s.group)
+        # #for g in groups:
+            
+    # #end finalize()
+
+
+
+# class PlotItem:
+    # def __init__(self, type, row, col, group):
+        # self.type = type
+        # self.relative_row = row
+        # self.relative_col = col
+        # self.samples = []
+    # #end __inti__()
+# #end class PlotItem
