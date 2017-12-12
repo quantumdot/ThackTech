1b571e0b0f90281637992fdd37c14a29288b741b
quantumdot <thackray@rutgers.edu>
2017-12-12 12:13:36 -0500

    parameter typo

--- ThackTech/Pipelines/PipelineModules/HISAT2Align.py
+++ ThackTech/Pipelines/PipelineModules/HISAT2Align.py
@@ -33,7 +33,7 @@ class HISAT2Align(PipelineModule):
 		self.add_parameter(ModuleParameter('min_intronlen', int, 20, desc="minimum intron length"))
 		self.add_parameter(ModuleParameter('max_intronlen', int, 500000, desc="maximum intron length"))
 		self.add_parameter(ModuleParameter('no_temp_splicesite', bool, False, desc="disable the use of splice sites found"))
-		self.add_parameter(ModuleParameter('rna_strandedness', str, 'unstranded', desc="specify strand-specific information"))
+		self.add_parameter(ModuleParameter('rna_strandness', str, 'unstranded', desc="specify strand-specific information"))
 		self.add_parameter(ModuleParameter('tmo', bool, False, desc="reports only those alignments within known transcriptome"))
 		self.add_parameter(ModuleParameter('dta', str, None, choices=['stringtie', 'cufflinks'], nullable=True, desc="Specify which, if any, downstream, transcript assemblers output should be tailored to. see --dta* option in HISAT2"))
 		self.add_parameter(ModuleParameter('avoid_pseudogene', bool, False, desc="tries to avoid aligning reads to pseudogenes; experimental!"))
