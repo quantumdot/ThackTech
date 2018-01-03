efeee61a056b566f805f53f31e977284463aaf81
quantumdot <thackray@rutgers.edu>
2018-01-03 13:45:55 -0500

    .

--- ThackTech/aligntools.py
+++ ThackTech/aligntools.py
@@ -44,6 +44,7 @@ def extract_barcode(fastq_path, complete=False):
     with pysam.FastxFile(fastq_path) as fh:
         for entry in fh:
             print entry.name
+            print entry.comment
             match = illumina14_18.search(entry.name)
             if match:
                 all_barcodes.add(match.group(1).upper())
