0c4399ba7d8759645ef7f9f9006e9152af262826
quantumdot <thackray@rutgers.edu>
2018-01-03 13:40:33 -0500

    added --all option to extract_barcode.py

--- scripts/extract_barcodes.py
+++ scripts/extract_barcodes.py
@@ -8,11 +8,12 @@ from ThackTech import filetools
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('fastq', nargs='+', help="fastq files to extract barcodes from")
+	parser.add_argument('--all', action='store_true', help="Report all barcodes found in the entire fastq. By default, only the first found is reported.")
 	args = parser.parse_args()
 	
 	for fq in args.fastq:
 		fq_name = filetools.basename_noext(fq, True)
-		sys.stdout.write("{}\t{}\n".format(fq_name, "\t".join(extract_barcode(fq, True))))
+		sys.stdout.write("{}\t{}\n".format(fq_name, "\t".join(extract_barcode(fq, args.all))))
 	
 #end main()
 
