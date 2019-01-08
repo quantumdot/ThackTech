e8194b7c2aec3157d86dd41b538094aa152bec70
quantumdot <thackray@rutgers.edu>
2019-01-08 15:40:39 -0500

    .

--- scripts/profiler.py
+++ scripts/profiler.py
@@ -678,8 +678,11 @@ def get_plot_axes(plot_type, group, bed_id, sig_id):
         #    col = gopts['fig_cols'] - 2
         #else:
         #    col = gopts['fig_cols'] - 1
-        col = max(gopts['plot_axes'].keys(), key=itemgetter(1))[0]
+        sys.stderr.write("plot_axes keys: {}".format(gopts['plot_axes'].keys()))
         row = max(gopts['plot_axes'].keys(), key=itemgetter(0))[0]
+        col = max(gopts['plot_axes'].keys(), key=itemgetter(1))[0]
+        sys.stderr.write("leg row col: {},{}".format(row, col))
+        
             
             
     if (row,col) not in gopts['plot_axes']:
