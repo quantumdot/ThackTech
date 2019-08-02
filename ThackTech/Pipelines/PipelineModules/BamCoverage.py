4e6053e52fd7ba037593b2a93826e7f87fa39e52
quantumdot <thackray@rutgers.edu>
2019-08-02 11:22:08 -0400

    fix args change, increase args coverage

--- ThackTech/Pipelines/PipelineModules/BamCoverage.py
+++ ThackTech/Pipelines/PipelineModules/BamCoverage.py
@@ -18,23 +18,23 @@ class BamCoverage(PipelineModule):
 		self.add_parameter(ModuleParameter('output_format', str, 'bigwig', choices=['bigwig', 'bedgraph'], desc="Output file format."))
 		
 		#Optional arguments
-		self.add_parameter(ModuleParameter('scale_factor', float, 1.0, desc=""))
-		self.add_parameter(ModuleParameter('MNase', bool, False))
-		self.add_parameter(ModuleParameter('offset'))
+		self.add_parameter(ModuleParameter('scale_factor', float, 1.0, desc="Scalar to muliply against the computed scaling factor"))
+		self.add_parameter(ModuleParameter('MNase', bool, False, desc="process as MNase data"))
+		self.add_parameter(ModuleParameter('offset', int, None, nullable=True))
 		self.add_parameter(ModuleParameter('filter_strand', str, None, nullable=True, choices=['forward', 'reverse'], desc=""))
-		self.add_parameter(ModuleParameter('region'))
-		self.add_parameter(ModuleParameter('blacklist'))
+		self.add_parameter(ModuleParameter('region', str, None, nullable=True))
+		self.add_parameter(ModuleParameter('blacklist', str, None, nullable=True))
 		self.add_parameter(ModuleParameter('bin_size', int, 1, desc="Size of the bins, in bases, for the output."))
 			
 		
 		#Read coverage normalization options:
-		self.add_parameter(ModuleParameter('normalize', str, 'rpkm', nullable=True, choices=['rpkm', '1x'], desc="Normalization type to perform"))
+		self.add_parameter(ModuleParameter('normalize', str, 'rpkm', nullable=True, choices=['RPKM','CPM','BPM','RPGC'], desc="Normalization type to perform"))
 		self.add_parameter(ModuleParameter('ignore_contigs', list, None, nullable=True, desc="list of contig (chromosome) names to exclude for normalization computation."))
 		self.add_parameter(ModuleParameter('skip_na', bool, False, desc="This parameter determines if non-covered regions (regions without overlapping reads) in a BAM file should be skipped."))
 		self.add_parameter(ModuleParameter('smooth', int, None, nullable=True, desc="The smooth length defines a window, larger than the binSize, to average the number of reads."))
 		
 		#Read processing options:
-		self.add_parameter(ModuleParameter('extend_reads'))
+		self.add_parameter(ModuleParameter('extend_reads', int, None, nullable=True))
 		self.add_parameter(ModuleParameter('ignore_duplicates', bool, False))
 		self.add_parameter(ModuleParameter('min_quality', int, None, nullable=True))
 		self.add_parameter(ModuleParameter('center_reads', bool, False))
@@ -51,7 +51,7 @@ class BamCoverage(PipelineModule):
 	def tool_versions(self):
 		bcp = self.get_parameter_value_as_string('bamCoverage_path')
 		return {
-			'bamCoverage': self._call_output(bcp+" --version 2>&1 | perl -ne 'if(m/([\d\.]+)/){ print $1 }'", shell=True, stderr=subprocess.STDOUT)
+			'bamCoverage': self._call_output(bcp+r" --version 2>&1 | perl -ne 'if(m/([\d\.]+)/){ print $1 }'", shell=True, stderr=subprocess.STDOUT)
 		}
 	#end tool_versions()
 	
@@ -74,10 +74,25 @@ class BamCoverage(PipelineModule):
 			'--outFileName', os.path.join(cxt.sample.dest, sample_basename),
 			'--outFileFormat', self.get_parameter_value_as_string('output_format'),
 			'--binSize', self.get_parameter_value_as_string('bin_size'),
-			'--normalizeUsingRPKM',
 			'--numberOfProcessors', str(self.processors),
+			'--scaleFactor', self.get_parameter_value_as_string('scale_factor'),
 			#'--verbose'
 		]
+		if self.get_parameter_value('normalize') is not None:
+			bamcoverage_args.extend(['--normalizeUsing', self.get_parameter_value_as_string('normalize')])
+
+		if self.get_parameter_value('MNase'):
+			bamcoverage_args.append('--MNase')
+
+		if self.get_parameter_value('filter_strand') is not None:
+			bamcoverage_args.extend(['--filterRNAstrand', self.get_parameter_value_as_string('filter_strand')])
+
+		if self.get_parameter_value('smooth') is not None:
+			bamcoverage_args.extend(['--smoothLength', self.get_parameter_value_as_string('smooth')])
+
+		if self.get_parameter_value('skip_na'):
+			bamcoverage_args.append('--skipNonCoveredRegions')
+
 		cxt.log.write('Generating normalized signal track....')
 		cxt.log.write("\n..............................................\n")
 		cxt.log.write(" ".join(bamcoverage_args))
@@ -86,7 +101,7 @@ class BamCoverage(PipelineModule):
 		self._run_subprocess(bamcoverage_args, cwd=cxt.sample.dest, stderr=subprocess.STDOUT, stdout=cxt.log)
 		
 		return {
-			'rpkm_norm_bw': os.path.join(cxt.sample.dest, sample_basename)
+			'coverage_signal': os.path.join(cxt.sample.dest, sample_basename)
 		}
 	#end run()
 #end class RPKMNormBigWig
