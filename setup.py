e536cc58124bef4b34838f7153e697bc18d39e2b
quantumdot <thackray@rutgers.edu>
2020-09-17 15:31:41 -0400

    update packaging

--- setup.py
+++ setup.py
@@ -1,50 +1,44 @@
-from setuptools import setup
+from distutils.core import setup
+import setuptools
+import os
+from pathlib import Path
 
+scripts = []
+script_dir = Path(__file__).resolve().parent.joinpath('ThackTech', 'scripts')
+for s in script_dir.glob('*.py'):
+    if s.name != '__init__.py':
+        ep = s.name.replace('.py', '')
+        scripts.append('{} = ThackTech.scripts.{}:main'.format(ep,ep))
 
-setup(  name='ThackTech',
-        version='0.1',
-        description='Libraries for process management, pipelineing, and other goodies.',
-        url='https://github.com/quantumdot/ThackTech',
-        author='Joshua K. Thackray',
-        author_email='thackray@rutgers.edu',
-        license='MIT',
-        install_requires=[
-            'numpy',
-            'scipy',
-            'pandas',
-            'matplotlib',
-            'metaseq',
-            'intervaltree',
-            'pysam',
-            'pybedtools',
-            'dill',
-            'multiprocess', #this is different than the built in multiprocessing!
-            'tabulate',
-            'pdfrw',
-            #'HTseq'
-            #'glob', 
-            #'sqlite3',
-        ],
-        scripts=[
-            "scripts/bedToBedGraph.py",
-            "scripts/wigToBedGraph.py",
-            "scripts/cleanBedGraph.py",
-            "scripts/profiler.py",
-            "scripts/profiler_info.py",
-            "scripts/correlator.py",
-            "scripts/bedstats.py",
-            "scripts/bowtie_alignment_pipeline.py",
-            "scripts/macs_peakcall_pipeline.py",
-            "scripts/epic_peakcall_pipeline.py",
-            "scripts/subsample_fastq.py",
-            "scripts/keggTermToGeneBed.py",
-            "scripts/analyze_radial_intensity.py",
-            "scripts/multi_venn.py",
-            "scripts/designCNVprobes.py",
-            "scripts/subsample_fastq.py",
-            "scripts/tuxedo2_pipeline.py",
-            "scripts/ginfo_inspect.py",
-            "scripts/manifest_manipulate.py",
-            "scripts/extract_barcodes.py"
-        ],
-        zip_safe=False)
+setup(
+    name='ThackTech',
+    version='0.1',
+    description='Libraries for process management, pipelineing, and other goodies.',
+    url='https://github.com/quantumdot/ThackTech',
+    author='Joshua K. Thackray',
+    author_email='thackray@rutgers.edu',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'numpy',
+        'scipy',
+        'pandas',
+        'matplotlib',
+        'metaseq',
+        'intervaltree',
+        'pysam',
+        'pybedtools',
+        'dill',
+        'multiprocess', #this is different than the built in multiprocessing!
+        'tabulate',
+        'pdfrw',
+        #'HTseq'
+        #'glob', 
+        #'sqlite3',
+    ],
+    entry_points={
+        'console_scripts': scripts,
+    },
+    zip_safe=False
+)
